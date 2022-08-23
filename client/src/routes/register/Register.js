85460657b0c8dd6bb41763088ab530eda5db08a4
william <william.mallak1@gmail.com>
2022-08-23 16:56:52 +0200

    we added the functionality for the form

--- client/src/routes/register/Register.js
+++ client/src/routes/register/Register.js
@@ -1,81 +1,107 @@
+import { useState } from "react";
 import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 
 export default function Register() {
+  const [registerForm, setRegisterForm] = useState({
+    username: "",
+    fname: "",
+    lname: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    isOwner: false
+  });
+
+  function changeHandler(e){
+    const element = e.target.name
+    const value = e.target.value
+    setRegisterForm((prevState) => {
+        return {...prevState, 
+            [element]: value 
+        }
+    })
+  }
+
   return (
     <div>
-        <h1>Register</h1>
-        <Form>
-  <FormGroup>
-    <Label for="username">
-      Username
+      <h1>Register</h1>
+      <Form onChange={(e) => changeHandler(e)}>
+        <FormGroup>
+          <Label for="exampleSelect">Select</Label>
+          <Input id="exampleSelect" name="isOwner" type="select">
+            <option value={false}>Car Owner</option>
+            <option value={true}>Wall-Box Owner</option>
+          </Input>
+        </FormGroup>
+        <FormGroup>
+          <Input name="username" placeholder="UserName" type="text" />
+        </FormGroup>
+        <FormGroup>
+          <Input name="fname" placeholder="First Name" type="text" />
+        </FormGroup>
+        <FormGroup>
+          <Input name="lname" placeholder="Last Name" type="text" />
+        </FormGroup>
+        <FormGroup>
+          <Input name="email" placeholder="Email" type="email" />
+        </FormGroup>
+        <FormGroup>
+          <Input name="password" placeholder="password" type="password" />
+        </FormGroup>
+        <FormGroup>
+          <Input
+            name="confirmPassword"
+            placeholder="password placeholder"
+            type="password"
+          />
+        </FormGroup>
+      </Form>
+
+      <Form /* style={{display: 'none'}} */>
+        <FormGroup className="d-flex">
+          <Input name="address" placeholder="street" />
+          <Input name="house number" placeholder="1" className="w-25" />
+        </FormGroup>
+        <FormGroup className="d-flex">
+          <Input name="zip code" placeholder="12345" />
+          <Input name="city" placeholder="city" />
+        </FormGroup>
+
+        <FormGroup>
+          <Label for="exampleSelectMulti">plugger</Label>
+          <Input id="plugger type" name="plugger type" type="select">
+            <option>plug 1</option>
+            <option>plug 2</option>
+            <option>plug 3</option>
+            <option>plug 4</option>
+            <option>plug 5</option>
+            <option>plug 6</option>
+          </Input>
+        </FormGroup>
+        {/* <FormGroup>
+    <Label for="exampleDate">
+      Date
     </Label>
     <Input
-      id="username"
-      name="email"
-      placeholder="UserName"
-      type="text"
+      id="exampleDate"
+      name="date"
+      placeholder="date placeholder"
+      type="date"
     />
   </FormGroup>
   <FormGroup>
-    <Label for="firstname">
-      First Name
+    <Label for="exampleTime">
+      Time
     </Label>
     <Input
-      id="firstname"
-      name="email"
-      placeholder="First Name"
-      type="email"
+      id="exampleTime"
+      name="time"
+      placeholder="time placeholder"
+      type="time"
     />
-  </FormGroup>
-  <FormGroup>
-    <Label for="lastname">
-      Last Name
-    </Label>
-    <Input
-      id="lastname"
-      name="email"
-      placeholder="Last Name"
-      type="email"
-    />
-  </FormGroup>
-  <FormGroup>
-    <Label for="exampleEmail">
-      Email
-    </Label>
-    <Input
-      id="exampleEmail"
-      name="email"
-      placeholder="with a placeholder"
-      type="email"
-    />
-  </FormGroup>
-  <FormGroup>
-    <Label for="examplePassword">
-      Password
-    </Label>
-    <Input
-      id="examplePassword"
-      name="password"
-      placeholder="password"
-      type="password"
-    />
-  </FormGroup>
-  <FormGroup>
-    <Label for="Confirm Password">
-    Confirm Password
-    </Label>
-    <Input
-      id="Confirm Password"
-      name="Confirm Password"
-      placeholder="password placeholder"
-      type="password"
-    />
-  </FormGroup>
-  
-  <Button>
-    Submit
-  </Button>
-</Form>
+  </FormGroup> */}
+      </Form>
+      <Button>Submit</Button>
     </div>
-  )
+  );
 }
