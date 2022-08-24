8d62db3cb6aa8d9f3c8a706270fc0ce2c4885bca
william <william.mallak1@gmail.com>
2022-08-24 17:48:04 +0200

    changed the address handler and added the provinces

--- client/src/routes/auth/Register.js
+++ client/src/routes/auth/Register.js
@@ -2,19 +2,17 @@ import { useState } from "react";
 import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 import { GermanyDataSet } from "../../dataset/GermanyDataSet.js";
 
-
-
-
 export default function Register() {
-  const [address, setAddress] = useState({
-    city: '',
+  const address = {
+    street: "",
+    houseNumber: "",
     postalCode: 0,
-    street: '',
-    houseNumber: '',
-    state: 'Germany',
-    stateCode: 'DE',
-    province: 'initial',
-})
+    city: "",
+    province: "initial",
+    state: "Germany",
+    stateCode: "DE",
+  };
+
   const [registerForm, setRegisterForm] = useState({
     username: "",
     fname: "",
@@ -23,34 +21,26 @@ export default function Register() {
     password: "",
     confirmPassword: "",
     isOwner: false,
-    address
+    address,
   });
- 
 
-    
-  function changeHandler(e){
-    const element = e.target.name
-    const value = e.target.value
+  function changeHandler(e) {
+    const element = e.target.name;
+    const value = e.target.value;
     setRegisterForm((prevState) => {
-        return {...prevState, 
-            [element]: value 
-        }
-    })
+      return { ...prevState, [element]: value };
+    });
   }
 
   function addressHandler(e) {
-    const element = e.target.name
-    const value = e.target.value
-    setAddress((prevState) => {
-      return {...prevState, 
-        [element]: value
-      }
-  })
+    const element = e.target.name;
+    const value = e.target.value;
+    address[element] = value;
+    registerForm.address = address;
+    console.log(registerForm);
   }
-  console.log(registerForm.address.province);
 
-  console.log(address.province);
-  
+  console.log(registerForm, "test");
 
   return (
     <div>
@@ -58,25 +48,55 @@ export default function Register() {
       <Form onChange={(e) => changeHandler(e)}>
         <FormGroup>
           <Label for="exampleSelect">Select</Label>
-          <Input id="exampleSelect" name="isOwner" type="select" autoComplete="">
+          <Input
+            id="exampleSelect"
+            name="isOwner"
+            type="select"
+            autoComplete=""
+          >
             <option value={false}>Car Owner</option>
             <option value={true}>Wall-Box Owner</option>
           </Input>
         </FormGroup>
         <FormGroup>
-          <Input name="username" placeholder="UserName" type="text" autoComplete=""/>
+          <Input
+            name="username"
+            placeholder="UserName"
+            type="text"
+            autoComplete=""
+          />
         </FormGroup>
         <FormGroup>
-          <Input name="fname" placeholder="First Name" type="text" autoComplete=""/>
+          <Input
+            name="fname"
+            placeholder="First Name"
+            type="text"
+            autoComplete=""
+          />
         </FormGroup>
         <FormGroup>
-          <Input name="lname" placeholder="Last Name" type="text" autoComplete=""/>
+          <Input
+            name="lname"
+            placeholder="Last Name"
+            type="text"
+            autoComplete=""
+          />
         </FormGroup>
         <FormGroup>
-          <Input name="email" placeholder="Email" type="email" autoComplete=""/>
+          <Input
+            name="email"
+            placeholder="Email"
+            type="email"
+            autoComplete=""
+          />
         </FormGroup>
         <FormGroup>
-          <Input name="password" placeholder="password" type="password" autoComplete=""/>
+          <Input
+            name="password"
+            placeholder="password"
+            type="password"
+            autoComplete=""
+          />
         </FormGroup>
         <FormGroup>
           <Input
@@ -86,51 +106,88 @@ export default function Register() {
             autoComplete=""
           />
         </FormGroup>
-      {registerForm.isOwner && 
-      
-      <>
-        <FormGroup>
-        <Label for="exampleSelect">Select</Label>
-          <Input id="exampleSelect" name="isOwner" type="select" autoComplete="">
-            <option value={false}>Car Owner</option>
-            <option value={true}>Wall-Box Owner</option>
-          </Input>
-        </FormGroup>
-        <FormGroup>
-        <Label for="exampleSelect">Select</Label>
-          <Input id="exampleSelect" name="isOwner" type="select" autoComplete="">
-            <option value={false}>Car Owner</option>
-            <option value={true}>Wall-Box Owner</option>
-          </Input>
-        </FormGroup>
-      </>
-      }
+        {registerForm.isOwner && (
+          <>
+            <FormGroup>
+              <Label for="exampleSelect">availability</Label>
+              <Input
+                id="exampleSelect"
+                name="isOwner"
+                type="select"
+                autoComplete=""
+              >
+                <option>all week</option>
+                <option>weekends</option>
+              </Input>
+            </FormGroup>
+            <FormGroup>
+              <Label for="exampleSelect">plug type</Label>
+              <Input
+                id="exampleSelect"
+                name="isOwner"
+                type="select"
+                autoComplete=""
+              >
+                <option>CCS</option>
+                <option>CHAdeMO</option>
+                <option>Tesla</option>
+              </Input>
+            </FormGroup>
+          </>
+        )}
       </Form>
-      <Form>
-        {registerForm.isOwner && 
-        <FormGroup onChange={(e) => addressHandler(e)}>
-        <Label for="exampleSelect">Select</Label>
-        <Input id="exampleSelect" name="province" type="select" autoComplete="">
-          <option value={GermanyDataSet.BadenWürttemberg} >Baden-Württemberg</option>
-          <option value={GermanyDataSet.Bavaria} >Bavaria</option>
-          <option></option>
-          <option></option>
-          <option></option>
-          <option></option>
-          <option></option>
-          <option></option>
-          <option></option>
-          <option></option>
-          <option></option>
-          <option></option>
-          <option></option>
-          <option></option>
-          <option></option>
-          <option></option>
-          {/* <option value={true}>Wall-Box Owner</option> */}
-        </Input>
-      </FormGroup>}
-      </Form>
+      {registerForm.isOwner && (
+        <Form>
+          <FormGroup onChange={(e) => addressHandler(e)}>
+            <Label for="exampleSelect">Select Province</Label>
+            <Input
+              id="exampleSelect"
+              name="province"
+              type="select"
+              autoComplete=""
+            >
+              <option value={GermanyDataSet.BadenWürttemberg}>
+                Baden-Württemberg
+              </option>
+              <option value={GermanyDataSet.Bavaria}>Bavaria</option>
+              <option value={GermanyDataSet.Berlin}>Berlin</option>
+              <option value={GermanyDataSet.Brandenburg}>Brandenburg</option>
+              <option value={GermanyDataSet.Bremen}>Bremen</option>
+              <option value={GermanyDataSet.Hamburg}>Hamburg</option>
+              <option value={GermanyDataSet.Hesse}>Hesse</option>
+              <option value={GermanyDataSet.LowerSaxony}>LowerSaxony</option>
+              <option value={GermanyDataSet.MecklenburgVorpommern}>
+                MecklenburgVorpommern
+              </option>
+              <option value={GermanyDataSet.NorthRhineWestphalia}>
+                NorthRhineWestphalia
+              </option>
+              <option value={GermanyDataSet.RhinelandPalatinate}>
+                RhinelandPalatinate
+              </option>
+              <option value={GermanyDataSet.Saarland}>Saarland</option>
+              <option value={GermanyDataSet.Saxony}>Saxony</option>
+              <option value={GermanyDataSet.SaxonyAnhalt}>SaxonyAnhalt</option>
+              <option value={GermanyDataSet.SchleswigHolstein}>
+                SchleswigHolstein
+              </option>
+              <option value={GermanyDataSet.Thuringia}>Thuringia</option>
+            </Input>
+          </FormGroup>
+          <FormGroup onChange={(e) => addressHandler(e)}>
+            <Input name="street" type="text" placeholder="Street" />
+          </FormGroup>
+          <FormGroup onChange={(e) => addressHandler(e)}>
+            <Input name="houseNumber" type="text" placeholder="0" />
+          </FormGroup>
+          <FormGroup onChange={(e) => addressHandler(e)}>
+            <Input name="postalCode" type="number" placeholder="Postal Code" />
+          </FormGroup>
+          <FormGroup onChange={(e) => addressHandler(e)}>
+            <Input name="city" type="text" placeholder="city" />
+          </FormGroup>
+        </Form>
+      )}
       <Button>sign up</Button>
     </div>
   );
