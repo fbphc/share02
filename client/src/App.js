c3a833e6e9b93fa14bef60c501bb4e83d7151eb2
str_hc <fbpetrella@gmail.com>
2022-08-24 14:21:00 +0200

    .

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const fname = "lname";
+  const object={fname}
+
+
+
   return (
     <div className="App">
       <h1>Final Project</h1>
--- server/server.js
+++ server/server.js
@@ -2,11 +2,17 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 
+import userRouter from "./routes/userRouter.js";
+
+
 import connectDB from "./helpers/dbConnect.js"
 import mongoose from "mongoose";
 
 const app = express();
 const PORT = process.env.PORT || 5007;
+
+app.use(cors());
+app.use(express.json());
 
 /* --- DATABASE --- */
 connectDB()
@@ -20,11 +26,17 @@ mongoose.connection.on("error", (error)=>{
 
 /* --------------- */
 
+
+/* --- Testing ---*/
 app.get("/test", (req,res) =>{
     res.status(200).json("testing route")
 })
-app.get("/testMergingFabio", (req,res) =>{
-    res.status(200).json("testing MergingFabio")
-})
+/* --------------- */
+
+/* --- User ---*/
+app.use("/user", userRouter)
+/* --------------- */
+
+
 
 app.listen(PORT, (req, res) => console.log("Listening at port:", PORT));
