e5d993b08d3de60369c0a04f196150552b34892e
str_hc <fbpetrella@gmail.com>
2022-08-24 17:13:22 +0200

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,35 @@
-import logo from "./logo.svg";
-import "./App.css";
+// styles
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+// libraries
+import {Routes, Route} from 'react-router-dom';
+
+// routes
+import HomePage from './components/routes/homePage/HomePage.js';
+import AboutUs from './components/routes/about-us/AboutUs.js';
+import Login from './components/routes/auth/Login.js';
+import ContactUs from './components/routes/contact/ContactUs.js';
+
+// components
+import NavBar from './components/navigation/NavBar.js';
+import Register from './components/routes/auth/Register.js';
+
+
 
 function App() {
-  const fname = "lname";
-  const object={fname}
-
-
-
   return (
     <div className="App">
-      <h1>Final Project</h1>
+      <NavBar/>
+      {/* <BrowserRouter> */}
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/aboutus' element={<AboutUs/>} />
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/contact' element={<ContactUs/>}/>
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
+      {/* </BrowserRouter> */}
     </div>
   );
 }
