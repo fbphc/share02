83438db7a18aacd9e46854a14f8afe0c8ecb0cb0
william <william.mallak1@gmail.com>
2022-08-30 09:26:57 +0200

    before pulling from fabio

--- client/src/components/navigation/Footer.js
+++ client/src/components/navigation/Footer.js
@@ -3,6 +3,6 @@ import React from 'react'
 export default function Footer() {
   return (
       // styling only for me
-    <div style={{border: "solid 1px red", position: "fixed", bottom: "0", width: "100%", height: "2rem"}}>Footer I don't know what it should contain :)</div>
+    <div style={{border: "solid 1px red"}}>Footer I don't know what it should contain :)</div>
   )
 }
