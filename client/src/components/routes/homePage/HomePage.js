c9e39858bfb4b85722ff063a6c7e590944c9a6ba
isra <isragonher@gmail.com>
2022-08-31 12:27:54 +0200

    pushing img-data from frontend to db

--- client/src/components/routes/homePage/HomePage.js
+++ client/src/components/routes/homePage/HomePage.js
@@ -1,30 +1,30 @@
-// import { useState } from "react";
-// import axios from "axios";
+import { useState } from "react";
+import axios from "axios";
 
-// export default function LandingPage() {
-//   const [selectedFile, setSelectedFile] = useState(null)
+export default function LandingPage() {
+    const [selectedFile, setSelectedFile] = useState(null)
 
-//   function fileHandler(e) {
-//     setSelectedFile(e.target.files[0]);
-   
-//   }
-//   function uploadHandler() {
-//     const fd = new FormData();
-//     fd.append('image', selectedFile, selectedFile.name)
-//     console.log(selectedFile);
-//     axios.post('http://localhost:5006/test', fd)
-//       .then(res => {
-//         console.log(res);
-//       })
-//   }
+    function fileHandler(e) {
+        setSelectedFile(e.target.files[0]);
 
-//   return (
-//     <div>
-//       Home page
-//       <div>
-//         <input type='file' onInput={(e) => fileHandler(e)} />
-//         <button onClick={uploadHandler}>Upload</button>
-//       </div>
-//     </div>
-//   )
-// }
+    }
+    function uploadHandler() {
+        const fd = new FormData();
+        fd.append('imageFile', selectedFile, selectedFile.name)
+        axios.post('http://localhost:5006/user/upload', fd, {
+        })
+            .then(res => {
+                console.log(res);
+            })
+    }
+
+    return (
+        <div>
+            Home page
+            <div>
+                <input type='file' name="imageFile" onInput={(e) => fileHandler(e)} />
+                <button onClick={uploadHandler}>Upload</button>
+            </div>
+        </div>
+    )
+}
