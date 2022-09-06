3346f408a8c9da2c1930e2e7d8ba68ba0dcc2945
Fabio Petrella <88140391+fbphc@users.noreply.github.com>
2022-09-06 11:19:20 +0200

    Update HomePage.js

--- client/src/components/routes/homePage/HomePage.js
+++ client/src/components/routes/homePage/HomePage.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 
 export default function LandingPage() {
-    const [selectedFile, setSelectedFile] = useState(null) // case reducer
+    const [selectedFile, setSelectedFile] = useState(null) 
 
     function fileHandler(e) {
         setSelectedFile(e.target.files[0]);
