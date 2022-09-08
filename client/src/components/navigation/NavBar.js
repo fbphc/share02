470a9b916956be647030c8cfb3cf5a44b75e4bc9
william <william.mallak1@gmail.com>
2022-09-08 11:41:51 +0200

    pulling after cleaning

--- client/src/components/navigation/NavBar.js
+++ client/src/components/navigation/NavBar.js
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Offcanvas, OffcanvasBody, OffcanvasHeader } from "reactstrap";
-import { BiMenu } from "react-icons/bi";
+import { Button, CardImg, Dropdown, DropdownItem, DropdownMenu, DropdownToggle, Offcanvas, OffcanvasBody, OffcanvasHeader } from "reactstrap";
+import { FaBars } from "react-icons/fa";
 import { Link, useLocation } from "react-router-dom";
 import useAuth from "../../context/authContext/useAuth.js";
+import "../../styles/NavBar.Module.css";
 
 export default function NavBar() {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggle = () => setDropdownOpen(!dropdownOpen);
+
   const [show, setShow] = useState(false);
   function closeMenu() {
     setShow(false);
@@ -14,6 +19,9 @@ export default function NavBar() {
   /* --- check the validation --- */
   const { tokenValidator, signOut, isAuthenticated } = useAuth();
   const location = useLocation();
+
+  const isLoggedIn = localStorage.getItem("user");
+
   useEffect(() => {
     const user = localStorage.getItem("user");
     if (user) {
@@ -22,24 +30,26 @@ export default function NavBar() {
   }, [location.pathname]);
   /* ------------- */
 
+  
+
   function loggingOut() {
     signOut();
     setShow(false);
   }
   return (
-    <div>
+    <div className="d-flex align-items-center justify-content-between navbarContainer">
       <div>
         <img src={""} alt="" />
       </div>
       <div>
-        <Button
-          color="primary"
+        <button
+          className="burgerMenuBtn bg-transparent border-0"
           onClick={function noRefCheck() {
             setShow(true);
           }}
         >
-          <BiMenu />
-        </Button>
+          <FaBars className="burgerMenuIcon" />
+        </button>
         <Offcanvas
           isOpen={show}
           direction="end"
@@ -54,6 +64,34 @@ export default function NavBar() {
           >
             <Button onClick={loggingOut}>sign out</Button>
           </OffcanvasHeader>
+          <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+            <DropdownToggle nav caret className="w-50">
+              {/* added */}
+              <CardImg
+              className="rounded-circle w-25"
+                src={"https://github.com/mshaaban0.png"}
+                alt="UserName profile image"
+              />
+              {/* <FaUserCircle/> */}
+            </DropdownToggle>
+            <DropdownMenu>
+              {isLoggedIn ? (
+                <DropdownItem>
+                  <Link
+                    onClick={closeMenu}
+                    to="profile"
+                    style={{ textDecoration: "none", color: "black" }}
+                  >
+                    profile preferences
+                  </Link>
+                </DropdownItem>
+              ) : (
+                <Link onClick={closeMenu} to="login">
+                  Login
+                </Link>
+              )}
+            </DropdownMenu>
+          </Dropdown>
           <OffcanvasBody>
             <strong className="linksContainer">
               {/* link imported from react dom class name to  */}
