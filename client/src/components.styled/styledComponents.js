4e64318699c43c7386e219f2841bf6b4928fdeaa
isra <isragonher@gmail.com>
2022-09-14 09:02:48 +0200

    styled home aboutus

--- client/src/components.styled/styledComponents.js
+++ client/src/components.styled/styledComponents.js
@@ -1,14 +1,12 @@
 import styled from "styled-components";
 
 export const MainButton = styled.button`
-  
   padding: 0.4rem 0.5rem;
   border-radius: 0.4rem;
   border: 1px solid var(--danger-color);
   background-color: transparent;
   color: var(--dark-color);
   font-weight: bold;
-
   transition: all 0.3s;
   &:hover {
     background-color: var(--danger-color);
@@ -16,5 +14,31 @@ export const MainButton = styled.button`
   }
   &:active {
     transform: scale(1.1);
+  }
+`;
+
+// home page
+export const MapImg = styled.img`
+  @media (max-width: 1100px) {
+    margin-top: 10%;
+    margin-bottom: 15%
+  }
+  @media (max-width: 700px) {
+    margin-top: 30%;
+    margin-bottom: 30%
+  }
+`;
+
+// about us
+export const ImgAbout = styled.img`
+  width: 100%;
+  height: 400px;
+  object-fit: cover
+`;
+
+export const ImgChargers = styled.img`
+  width: 100%;
+  @media (min-width: 900px) {
+    width: 70%
   }
 `;
--- client/src/components/routes/homePage/HomePage.js
+++ client/src/components/routes/homePage/HomePage.js
@@ -1,15 +1,16 @@
 import React from "react";
 import mapImg from "../../../img/mapImg.png";
+import { MapImg } from "../../../components.styled/styledComponents"
 
 function LandingPage() {
   return (
     <>
-      
-      <div className="position-realtive w-75 mx-auto mt-5 imageCSS">
-        <img src={mapImg} alt="landing-map" className="opacity-75 w-100" />
+
+      <div className="position-realtive w-75 mx-auto mt-2">
+        <MapImg src={mapImg} alt="landing-map" className="opacity-75 w-100" />
       </div>
-      <div className="text-center mt-5">
-        <p className="my-3 h1 text-light"> WELCOME TO <br></br>EV-<span className="text-warning"><b>C</b></span>ONNECTION</p>
+      <div className="text-center">
+        <p className="h1 text-light"> WELCOME TO <br></br>EV-<span className="text-warning"><b>C</b></span>ONNECTION</p>
       </div>
     </>
   );
